42fb85ca032f915530622d50d1a1e24614bbea80
Timothy Ngotiaoco <timothy_ngotiaoco@harvard.edu>
2024-10-01 13:16:47 -0400

    Update SLURM script with ray address

--- server/405b_slurm.sh
+++ server/405b_slurm.sh
@@ -16,6 +16,12 @@ module load python/3.10.13-fasrc01
 conda deactivate
 conda activate vllm-inference
 
+if [[ -z $SLURM_GPUS_ON_NODE ]]; then
+    RAY_NUM_GPUS=0
+else
+    RAY_NUM_GPUS=$SLURM_GPUS_ON_NODE
+fi
+
 # choose available port on the head node
 head_port=`comm -23 <(seq 15000 20000 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf | head -n 1`
 nodes=`scontrol show hostnames $SLURM_JOB_NODELIST`
@@ -25,23 +31,23 @@ head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
 echo "Head node: $head_node"
 echo "Head node ip: $head_node_ip"
 echo "Head port: $head_port"
-export head_addr="$head_node_ip:$head_port"
-echo "Head address: $head_addr"
+export RAY_HEAD_ADDR="$head_node_ip:$head_port"
+echo "Head address: $RAY_HEAD_ADDR"
 
 echo "Starting Ray head on $head_node"
 srun -N 1 -n 1 -w "$head_node" ray start --head --node-ip-address="$head_node_ip" --temp-dir /tmp/$USER/$SLURM_JOB_ID/ray \
-    --port=$head_port --num-cpus $SLURM_CPUS_PER_TASK --num-gpus $SLURM_GPUS_PER_NODE --min-worker-port 20001 --max-worker-port 30000 --block &
+    --port=$head_port --num-cpus $SLURM_CPUS_PER_TASK --num-gpus $RAY_NUM_GPUS --min-worker-port 20001 --max-worker-port 30000 --block &
 
 # wait for head node to start
-sleep 30
+sleep 5
 
 # start ray on the rest of the nodes
 worker_num=$((SLURM_NNODES - 1))
 for (( i = 1; i <= worker_num; i++ )); do
     node=${nodes_array[$i]}
     echo "Starting Ray worker on $node"
-    srun -N 1 -n 1 -w "$node" ${SINGULARITY_WRAP} ray start --address="$head_addr" \
-        --num-cpus $SLURM_CPUS_PER_TASK --num-gpus $SLURM_GPUS_PER_NODE --min-worker-port 20001 --max-worker-port 30000 --block &
+    srun -N 1 -n 1 -w "$node" ray start --address="$RAY_HEAD_ADDR" \
+        --num-cpus $SLURM_CPUS_PER_TASK --num-gpus $RAY_NUM_GPUS --min-worker-port 20001 --max-worker-port 30000 --block &
     sleep 5
 done
 
